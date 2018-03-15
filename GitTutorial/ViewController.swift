7c06f35b66a2962f120a3ae53669522d14832aec
Raducu Filip <raducufilip@MacBook-Pro-de-Raducu.local>
2018-03-15 15:23:27 +0200

    added

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -11,14 +11,13 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+                print("bla")
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
